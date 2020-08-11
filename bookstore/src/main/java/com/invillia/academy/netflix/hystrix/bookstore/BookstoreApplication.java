c82e55edaff51a5279f68cae3da504d9d1ef54ad
Tuanny <ramostuanny@gmail.com>
2020-08-11 07:44:45 -0300

    adds log to bookstore

--- bookstore/src/main/java/com/invillia/academy/netflix/hystrix/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/invillia/academy/netflix/hystrix/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package com.invillia.academy.netflix.hystrix.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import java.util.List;
 @RestController
 @SpringBootApplication
 public class BookstoreApplication {
+	Logger logger = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	@RequestMapping(value = "/recommended")
 	public String bookList() {
@@ -18,6 +21,7 @@ public class BookstoreApplication {
 		books.add("Spring in Action (Manning)");
 		books.add("Cloud Native Java (O'Reilly)");
 		books.add("Learning Spring Boot (Packt)");
+		logger.info("acessando livros recomendados");
 
 		return String.join(", ", books);
 	}
