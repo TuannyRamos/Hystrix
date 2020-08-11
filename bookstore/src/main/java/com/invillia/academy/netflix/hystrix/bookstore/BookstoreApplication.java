a6734009b69c595e98134194c48b8668e7785d57
Tuanny <ramostuanny@gmail.com>
2020-08-11 07:31:54 -0300

    bookstore application

--- bookstore/src/main/java/com/invillia/academy/netflix/hystrix/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/invillia/academy/netflix/hystrix/bookstore/BookstoreApplication.java
@@ -2,9 +2,25 @@ package com.invillia.academy.netflix.hystrix.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class BookstoreApplication {
+
+	@RequestMapping(value = "/recommended")
+	public String bookList() {
+		List<String> books = new ArrayList<>();
+		books.add("Spring in Action (Manning)");
+		books.add("Cloud Native Java (O'Reilly)");
+		books.add("Learning Spring Boot (Packt)");
+
+		return String.join(", ", books);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
