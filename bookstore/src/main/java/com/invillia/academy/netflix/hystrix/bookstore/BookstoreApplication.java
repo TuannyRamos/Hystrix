2302e9fda572c5e56496368298bef40121ccecfd
Tuanny <ramostuanny@gmail.com>
2020-08-11 07:17:09 -0300

    initial commit

---
+++ bookstore/src/main/java/com/invillia/academy/netflix/hystrix/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.invillia.academy.netflix.hystrix.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
---
+++ bookstore/src/test/java/com/invillia/academy/netflix/hystrix/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.academy.netflix.hystrix.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
