1630e3283e849130acbeb75eb78730a86fa058fe
Tuanny <ramostuanny@gmail.com>
2020-08-11 07:45:09 -0300

    reader application

--- consumer/src/main/java/com/invillia/academy/netflix/hystrix/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/invillia/academy/netflix/hystrix/consumer/ConsumerApplication.java
@@ -1,10 +1,30 @@
 package com.invillia.academy.netflix.hystrix.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RestController
 @SpringBootApplication
 public class ConsumerApplication {
+
+	Logger logger = LoggerFactory.getLogger(ConsumerApplication.class);
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/to-read")
+	public String toRead() {
+		logger.info("chamando /recommended API");
+		return this.restTemplate().getForObject("http://localhost:8090/recommended", String.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
