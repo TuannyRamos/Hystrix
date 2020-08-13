5503339c9ab31e28ff56f2c373aeac91694935e0
Tuanny <ramostuanny@gmail.com>
2020-08-13 14:50:33 -0300

    adds annotation

--- consumer/src/main/java/com/invillia/academy/netflix/hystrix/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/invillia/academy/netflix/hystrix/consumer/ConsumerApplication.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableRetry
 @RestController
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ConsumerApplication {
 
